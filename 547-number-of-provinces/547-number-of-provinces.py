304a69c7bac7d25616c72fa97b8f91416071b1d5
mohith <50114158+mohithkailash@users.noreply.github.com>
2022-08-27 11:45:45 -0700

    Time: 2100 ms (5.00%), Space: 24.2 MB (5.03%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -2,6 +2,7 @@ class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         rows = len(isConnected)
         cols = len(isConnected[0])
+        visited = set()
         count = 0
         def helper(i,j):
             if i < 0 or i >= rows or j < 0 or j >= cols:
@@ -12,13 +13,16 @@ class Solution:
                 if isConnected[i][j] == 1:
                     isConnected[i][j] = 2
                     if i != j:
+                        visited.add(j)
                         helper(j,0)
                 j += 1
         for i in range(rows):
-            for j in range(cols):
-                if isConnected[i][j] == 1:
-                    count += 1
-                    helper(i,j)
+            if i not in visited:
+                visited.add(i)
+                for j in range(cols):
+                    if isConnected[i][j] == 1:
+                        count += 1
+                        helper(i,j)
         
         return count
                 
