b2167896ff202e7338a06f8742d9304628f96de2
mohith <50114158+mohithkailash@users.noreply.github.com>
2022-09-04 21:10:18 -0700

    Time: 97 ms (76.60%), Space: 14.2 MB (73.57%) - LeetHub

--- 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.py
+++ 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.py
@@ -1,19 +1,29 @@
 class Solution:
     def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
-        i , j = 0 , 0
-        ans = 0
-        check = {}
-        while i < len(s) and j < len(s):
-            if s[j] not in check:
-                check[s[j]] = 0
-            check[s[j]] += 1
-            while len(check) > k:
-                check[s[i]] -= 1
-                if check[s[i]] == 0:
-                    del check[s[i]]
-                i += 1
-            ans = max(ans, j-i+1)
-            j += 1
+        ans=0
+        start=0
+        dic={}
+        for i in range(len(s)):
+            dic[s[i]]=i
+            if len(dic)>k:
+                start=min(dic.values())+1
+                del dic[s[start-1]]
+            ans=max(ans,i-start+1)
         return ans
+        # i , j = 0 , 0
+        # ans = 0
+        # check = {}
+        # while i < len(s) and j < len(s):
+        #     if s[j] not in check:
+        #         check[s[j]] = 0
+        #     check[s[j]] += 1
+        #     while len(check) > k:
+        #         check[s[i]] -= 1
+        #         if check[s[i]] == 0:
+        #             del check[s[i]]
+        #         i += 1
+        #     ans = max(ans, j-i+1)
+        #     j += 1
+        # return ans
             
             
