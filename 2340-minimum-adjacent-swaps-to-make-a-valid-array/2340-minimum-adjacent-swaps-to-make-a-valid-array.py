dd402ee2f79d348cf9e974c83664df546aba09c9
mohith <50114158+mohithkailash@users.noreply.github.com>
2022-09-13 13:03:03 -0700

    Time: 1173 ms (9.47%), Space: 27.7 MB (84.60%) - LeetHub

---
+++ 2340-minimum-adjacent-swaps-to-make-a-valid-array/2340-minimum-adjacent-swaps-to-make-a-valid-array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minimumSwaps(self, nums: List[int]) -> int:
+        minpos = maxpos = 0
+        minval = maxval = nums[0]
+        for i in range(len(nums)):
+            if nums[i] < minval:
+                minpos = i
+                minval = nums[i]
+            if nums[i] >= maxval:
+                maxpos = i
+                maxval = nums[i]
+        # for i in range(len(nums)-1,-1,-1):
+        #     if nums[i] > maxval:
+        #         maxpos = i
+        left = minpos
+        right = len(nums) - (1 + maxpos)
+        print(left,maxpos)
+        if minpos > maxpos:
+            return right + left - 1
+        else:
+            return right + left
